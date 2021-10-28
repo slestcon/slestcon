43289733ce36957e72e49a60f2d93f9899140f61
moogleberry <uremog@gmail.com>
2021-10-28 13:10:39 -0400

    added basic pages for 2021 and 2022

---
+++ src/components/Slestcon2021/Slestcon2021.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Slestcon2021 = () => (
+	<div>
+		<h2 className="page-title">
+        SlestCon 2021
+		</h2>
+		
+		<div>
+			
+		</div>
+	</div>
+);
+
+export default Home;
---
+++ src/components/Slestcon2022/Slestcon2022.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Slestcon2022 = () => (
+	<div>
+		<h2 className="page-title">
+        SlestCon 2022
+		</h2>
+		
+		<div>
+			
+		</div>
+	</div>
+);
+
+export default Home;
