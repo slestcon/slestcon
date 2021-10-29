6c6e458901af6bf07d3b740194ff33e37821698b
moogleberry <uremog@gmail.com>
2021-10-29 15:58:44 -0400

    attempting to use hashrouter

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -9,12 +9,11 @@ const Navigation = () => (
 			<Navbar.Toggle aria-controls="basic-navbar-nav"/>
 			<Navbar.Collapse id="basic-navbar-nav">
 				<Nav className="me-auto">
-					<Nav.Link href="/Slestcon2021">SlestCon 2021</Nav.Link>
-					<Nav.Link href="/Slestcon2022">SlestCon 2022</Nav.Link>
-					
+					<Nav.Link href="/#/Slestcon2021">SlestCon 2021</Nav.Link>
+					<Nav.Link href="/#/Slestcon2022">SlestCon 2022</Nav.Link>
 				</Nav>
 				<Nav className="d-flex">
-					<Nav.Link href="/Contact">Contact</Nav.Link>
+					<Nav.Link href="/#/Contact">Contact</Nav.Link>
 				</Nav>
 			</Navbar.Collapse>
 		</Container>
--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import './index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/slestcon">
+    <HashRouter basename="/slestcon">
 			<App />
-		</BrowserRouter>
+		</HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
