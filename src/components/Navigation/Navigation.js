43ae9a2e9d79ab09326e8aca0c005b25e8461d77
moogleberry <uremog@gmail.com>
2021-10-29 16:05:05 -0400

    trying new links still with router basename

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,15 +5,15 @@ const Navigation = () => (
 
 	<Navbar bg="dark" variant="dark" sticky="top" expand="lg">
 		<Container>
-			<Navbar.Brand href="/">SlestCon</Navbar.Brand>
+			<Navbar.Brand href="/slestcon/#">SlestCon</Navbar.Brand>
 			<Navbar.Toggle aria-controls="basic-navbar-nav"/>
 			<Navbar.Collapse id="basic-navbar-nav">
 				<Nav className="me-auto">
-					<Nav.Link href="/#/Slestcon2021">SlestCon 2021</Nav.Link>
-					<Nav.Link href="/#/Slestcon2022">SlestCon 2022</Nav.Link>
+					<Nav.Link href="/slestcon/#/Slestcon2021">SlestCon 2021</Nav.Link>
+					<Nav.Link href="/slestcon/#/Slestcon2022">SlestCon 2022</Nav.Link>
 				</Nav>
 				<Nav className="d-flex">
-					<Nav.Link href="/#/Contact">Contact</Nav.Link>
+					<Nav.Link href="/slestcon/#/Contact">Contact</Nav.Link>
 				</Nav>
 			</Navbar.Collapse>
 		</Container>
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
+    <HashRouter basename="/slestcon">
 			<App />
 		</HashRouter>
   </React.StrictMode>,
