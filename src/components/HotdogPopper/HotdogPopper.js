238a4756253ee09ac9625fc7449896c49c5474ed
moogleberry <uremog@gmail.com>
2021-11-09 20:54:38 -0500

    hotdog popper now cleans itself up

--- src/components/HotdogPopper/HotdogPopper.js
+++ src/components/HotdogPopper/HotdogPopper.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHotdog } from '@fortawesome/free-solid-svg-icons';
+import _ from 'lodash';
 
 import PoppedHotdog from "./PoppedHotdog";
 
@@ -11,40 +12,43 @@ class HotdogPopper extends React.Component {
 
         this.state = {
             popperConfig: {
-                minHotdogs: 4, // inclusive
-                maxHotdogs: 10, // not inclusive
+                minHotdogs: 6, // inclusive
+                maxHotdogs: 12, // not inclusive
                 minPopLength: 100, // inclusive
                 maxPopLength: 200, // not inclusive
-                disposeAfter: 1000,
+                disposeAfter: 1000, // ms
                 possibleColors: ["text-primary", "text-secondary", "text-success", "text-danger", "text-warning", "text-info", "text-muted"],
             },
             selfRef: React.createRef(),
-            hotdogsArray: []
+            hotdogsObject: {}
         };
+
+        this.nextHotdogId = 0;
     }
 
-    getCenterOfRef(reference) {
+    getSpawnPointFromRef(reference) {
         if(reference) {
             const bounds = reference.current.getBoundingClientRect();
             const spawnPoint = [ bounds.x, bounds.y ];
             return spawnPoint;
         }
         else {
-            console.error("No ref passed to getCenterOfRef");
+            console.error("No ref passed to getSpawnPointFromRef");
         }
     }
 
-    popHotdogs(atReference, popperConfig) {
-        if(atReference && popperConfig) {
-            const numHotdogs = Math.floor((Math.random() * (popperConfig.maxHotdogs - popperConfig.minHotdogs)) + popperConfig.minHotdogs);
-            const newHotdogs = [];
-            const spawnPoint  = this.getCenterOfRef(atReference);
+    popHotdogs(atReference) {
+        if(atReference) {
+            const numHotdogs = Math.floor((Math.random() * (this.state.popperConfig.maxHotdogs - this.state.popperConfig.minHotdogs)) + this.state.popperConfig.minHotdogs);
+            const newHotdogs = {};
+            const spawnPoint  = this.getSpawnPointFromRef(atReference);
             
             for(let i = 0; i < numHotdogs; i++) {
-                newHotdogs.push(this.createHotdog(spawnPoint, popperConfig));
+                newHotdogs[this.nextHotdogId] = this.createHotdog(spawnPoint, this.state.popperConfig);
+                this.nextHotdogId++;
             }
 
-            this.addHotdogs(newHotdogs, popperConfig);
+            this.addHotdogs(newHotdogs, this.state.popperConfig);
         }
         else {
             console.error("Reference or Config missing from popHotdogs");
@@ -69,14 +73,16 @@ class HotdogPopper extends React.Component {
         return newHotdog;
     }
 
-    addHotdogs(newHotdogs, popperConfig) {
-        this.setState({ hotdogsArray: this.state.hotdogsArray.concat(newHotdogs) });
-        setTimeout(() => {this.applyAllHotdogsTransition()});
-        setTimeout(() => {this.removeHotdogs()}, popperConfig.disposeAfter);
+    addHotdogs(newHotdogs) {
+        this.setState({ 
+            hotdogsObject: {...this.state.hotdogsObject, ...newHotdogs}
+        });
+        setTimeout(() => {this.applyTransitionToCurrentHotdogs()});
+        setTimeout(() => {this.hotdogRemoval(newHotdogs)}, this.state.popperConfig.disposeAfter);
     }
 
-    applyAllHotdogsTransition() {
-        const newHotdogsArray = this.state.hotdogsArray.map((hotdog) => {
+    applyTransitionToCurrentHotdogs() {
+        const newHotdogsObject = _.mapValues(this.state.hotdogsObject, (hotdog) => {
             hotdog.hotdogStyle = {
                 left: hotdog.spawnPoint[0] + (Math.cos(hotdog.popAngle) * hotdog.popLength),
                 top: hotdog.spawnPoint[1] + (Math.sin(hotdog.popAngle) * hotdog.popLength),
@@ -85,20 +91,26 @@ class HotdogPopper extends React.Component {
             return hotdog;
         });
 
-        this.setState({ hotdogsArray: newHotdogsArray });
+        this.setState({ hotdogsObject: newHotdogsObject });
     }
 
-    removeHotdogs() {
-        // TODO remove hotdog elements when finished
+    hotdogRemoval(addedHotdogs) {
+        let addedKeys = Object.keys(addedHotdogs);
+        let newHotdogsObject = _.omit(this.state.hotdogsObject, addedKeys);
+        this.setState({ hotdogsObject: newHotdogsObject });
     }
 
     render() {
         return (
             <span ref={ this.state.selfRef }>
-                <FontAwesomeIcon icon={ faHotdog } onClick={ () => this.popHotdogs(this.state.selfRef, this.state.popperConfig) } />
+                <FontAwesomeIcon icon={ faHotdog } onClick={ () => this.popHotdogs(this.state.selfRef) } />
                 {
-                    this.state.hotdogsArray.map((hotdog, index) => (
-                        <PoppedHotdog style={hotdog.hotdogStyle} color={hotdog.hotdogColor} key={index} />
+                    Object.keys(this.state.hotdogsObject).map((index) => (
+                        <PoppedHotdog 
+                            style={this.state.hotdogsObject[index].hotdogStyle} 
+                            color={this.state.hotdogsObject[index].hotdogColor} 
+                            key={index} 
+                        />
                     ))
                 }
             </span>
